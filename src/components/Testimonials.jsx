cf2bb5d2c626596b69d745a5d256504877f269d4
Taher Najim <taher.najim@yahoo.com>
2023-08-21 18:09:13 +0800

    added gradient background to Testimonials component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => {
   return (
     <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
 
-      <div /> {/* todo gradient */}
+      <div className="absolute z-[0] w-[65%] h-[65%] -right-[50%] rounded-full blue__gradient" />
 
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 z-[1]">
         <h1 className={styles.heading2}>What people are <br className="sm:block hidden" /> saying about us</h1>
