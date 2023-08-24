b2e5502e5778f9859ba0d8de697bd25093b2a0a6
Taher Najim <taher.najim@yahoo.com>
2023-08-24 16:48:55 +0800

    changed CTA header

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
       <div className="flex-1 flex flex-col">
-        <h2 className={styles.heading2}>Let's try our service now!</h2>
+        <h2 className={styles.heading2}>Try our service now!</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
       </div>
 
